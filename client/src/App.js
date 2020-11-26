055d75b9e08f0f57f77271f2b02c0cf1a22d6897
Bipul Kumar <kumar.bipulbose@gmail.com>
2020-11-26 13:16:18 -0500

    UI refactored

--- client/src/App.js
+++ client/src/App.js
@@ -49,14 +49,23 @@ class App extends Component {
     render() {
         return (
             <div className="container-fluid">
-                <div className="row">
+                <header className="row header">RETHINK</header>
+                <div className="row content">
                     <div className="col">
                         <h6>Shorten your URL</h6>
-                        <input type="text" onChange={event => this.seturl(event.target.value)}/>
-                        <button className="btn btn-primary" onClick={event => this.shortenUrl()}>Go</button>
+                        <div className="form-group">
+                            <input type="text" className="form-control" onChange={event => this.seturl(event.target.value)} placeholder="Enter your URL here"/>
+                        </div>
+                        <div>
+                            <button className="btn btn-primary" onClick={event => this.shortenUrl()}>Generate</button>
+                        </div>
                     </div>
+                    {this.state.shortendUrl != null && (
+                        <div className="m-4">
+                            <p className="">your shortend url is: <a href={this.state.shortendUrl} target="_blank">{this.state.shortendUrl}</a></p>
+                        </div>
+                    )}
                 </div>
-                <p className="">your shortend url is: <a href={this.state.shortendUrl} target="_blank">{this.state.shortendUrl}</a></p>
             </div>
         );
     }
